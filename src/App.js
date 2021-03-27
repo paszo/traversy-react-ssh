a5a887a686451ee4a81277d15a5325c4b06eeca2
paszo <paszo2008@gmail.com>
2021-03-27 15:04:47 +0100

    little change

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
         {showAddTask && <AddTask onAdd={addTask}/>}
         {tasks.length > 0 ? (
             <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
-        ) : 'No Tasks to show'}
+        ) : 'Unfortunately No Tasks to show'}
     </div>
 
   );
